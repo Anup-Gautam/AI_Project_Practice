8426e09d79dee8959d4a6b486264f55e1a83d30e
Anup-Gautam <84380379+Anup-Gautam@users.noreply.github.com>
2024-10-15 13:36:14 -0500

    Index Page Draft

--- Travel-Planner/src/create-trip/index.jsx
+++ Travel-Planner/src/create-trip/index.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function CreateTrip() {
-  return <div className="trip-container"></div>;
+  return (
+    <>
+      <div className="trip-container">
+        <h2>How you like your Travel?</h2>
+        <p> Just fill this form so that we can make your trips better!</p>
+      </div>
+
+      <div>
+        <div>
+          <h2> Where do you want to go?</h2>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default CreateTrip;
