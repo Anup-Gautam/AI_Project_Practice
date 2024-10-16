05ea3237c9dd0a25e82902763436e249843914f4
Anup-Gautam <84380379+Anup-Gautam@users.noreply.github.com>
2024-10-16 02:06:29 -0500

    Google AutoComplete Integrated

--- Travel-Planner/src/create-trip/index.jsx
+++ Travel-Planner/src/create-trip/index.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import { TextField } from "@mui/material";
+import { useState, React } from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
 function CreateTrip() {
+  const [place, setPlace] = useState();
   return (
     <>
       <div className="trip-container">
@@ -11,6 +14,23 @@ function CreateTrip() {
       <div>
         <div>
           <h2> Where do you want to go?</h2>
+          <GooglePlacesAutocomplete
+            apiKey={import.meta.env.GOOGLE_API_KEY}
+            selectProps={{
+              place,
+              onChange: (v) => {
+                setPlace(v);
+              },
+            }}
+          />
+          <h2> How manys days are you planning for your trip</h2>
+          <TextField
+            fullWidth
+            sx={{ width: "99%", padding: "10px", margin: "10px" }}
+            variant="outlined"
+          />
+
+          <h2>How rich are you?</h2>
         </div>
       </div>
     </>
